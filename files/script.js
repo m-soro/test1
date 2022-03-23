d7cb58964acb33fde3408e61fc3757d3dfb13fc2
Mark Soro <63247801+m-soro@users.noreply.github.com>
2022-03-23 19:21:30 -0400

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,18 @@
+let options = {
+  root: document.querySelector('#scrollArea'),
+  rootMargin: '0px',
+  threshold: 1.0
+}
+
+const observer = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('animate__animated animate__fadeInLeft animate__delay-2s ');
+    }
+  });
+});
+
+observer.observe(document.querySelector('.picture2'));
