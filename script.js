33afc3f9a2ae763eb64bac3ee433a8521aaa0ccf
Mark Soro <63247801+m-soro@users.noreply.github.com>
2022-03-26 20:52:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const element = document.querySelector(".picture2");
+// const element = document.querySelector(".picture2");
 element.style.visibility = "visible";
 
 const callback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       console.log(entry)
-      entry.target.classList.add('animate__animated','animate__fadeInLeft', 'animate__slower')
+//       entry.target.classList.add('animate__animated','animate__fadeInLeft', 'animate__slower')
+         entry.target.classList.add(effects)
     }
   })
 }
